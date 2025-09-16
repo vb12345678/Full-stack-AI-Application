770b83c53a06a1164758bb2b843a19e7cc206d09
Vernon Brighton <vernonbrighton@mail.weber.edu>
2025-09-16 14:23:57 -0600

    added openAI-api

---
+++ chatapi.py
@@ -0,0 +1,20 @@
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+my_api_key = os.getenv("OPENAI_API_KEY")
+print('My api key',my_api_key)
+
+
+client = OpenAI(api_key= my_api_key.strip())
+
+completion = client.chat.completions.create(
+  model="gpt-4o-mini",
+  messages=[
+      {"role":"system","content":"You are a helpful assistant"},
+    {"role": "user", "content": "Write a haiku about a cold day"}
+  ]
+)
+
+print("response:",completion.choices[0].message.content)
